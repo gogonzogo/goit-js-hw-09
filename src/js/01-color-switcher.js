2d553c5c3370ce2824d223553f7285ef86a7ed46
Jose Gonzales <jose.gonzales08@ymail.com>
2023-02-14 10:54:14 -0500

    task 1 and 2 update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const ref = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+}
 
+let backgroundColorTimer = null;
+
+ref.startBtn.addEventListener("click", handleStartBtnClick);
+
+function handleStartBtnClick(evt) {
+  setTimeout(() => {
+    backgroundColorTimer = setInterval(() => {
+      ref.body.style.cssText = (`background-color: ${getRandomHexColor()}`);
+    }, 1000)
+  },4);
+  ref.startBtn.disabled = true;
+  ref.stopBtn.disabled = false;
+}
+
+ref.stopBtn.addEventListener("click", handleStopBtnClick);
+
+function handleStopBtnClick(e) {
+  clearInterval(backgroundColorTimer);
+  ref.stopBtn.disabled = true;
+  ref.startBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
